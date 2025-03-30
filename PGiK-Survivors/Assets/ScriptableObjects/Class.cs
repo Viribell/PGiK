47bf3c6383304c5318af5327cd43312798b26a3a
Viribell <socciallist@interia.pl>
2025-03-30 19:15:26 +0200

    2nd bulk commit of sprint3's functions

---
+++ PGiK-Survivors/Assets/ScriptableObjects/Class.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu( fileName = "Class" )]
+public class Class : ScriptableObject {
+    [field: SerializeField] public SerializableDictionary<StatType, float> stats { get; private set; }
+    [field: SerializeField] public List<StatModifier> levelUpMods { get; private set; }
+
+    public Dictionary<StatType, Stat> GetStats() {
+        if ( stats.Count == 0 ) return null;
+
+        Dictionary<StatType, Stat> statsDict = new Dictionary<StatType, Stat>();
+
+        foreach( KeyValuePair<StatType, float> entry in stats ) {
+            Stat stat = new Stat( entry.Value );
+
+            statsDict.Add( entry.Key, stat );
+        }
+
+        return statsDict;
+    }
+
+    public List<StatModifier> GetLevelUpMods() {
+        if ( levelUpMods.Count == 0 ) return null;
+
+        List<StatModifier> modsList = new List<StatModifier>();
+
+        foreach(StatModifier mod in levelUpMods) {
+            StatModifier newMod = new StatModifier( mod.value, mod.type, mod.affectedStat );
+
+            modsList.Add( newMod );
+        }
+
+        return modsList;
+    }
+}
